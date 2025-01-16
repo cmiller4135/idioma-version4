53475f2fbc1a412bfbcd332f584b110734d985e0
cmiller4135 <chrismillerw@gmail.com>
2025-01-16 17:39:19 -0500

    pub 2

---
+++ src/pages/teach/TeachSub1.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TeachSub1 = () => {
+  return (
+    <div className="bg-white p-6 rounded-lg shadow-md border border-[#E63946]">
+      <h1>Hello from TeachSub1</h1>
+    </div>
+  );
+};
+
+export default TeachSub1;
---
+++ src/pages/tools/Sub1.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sub1 = () => {
+  return (
+    <div className="bg-white p-6 rounded-lg shadow-md border border-[#E63946]">
+      <h1>Hello from Sub1</h1>
+    </div>
+  );
+};
+
+export default Sub1;
